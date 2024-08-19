20aa6804ad478e216f8bd58957fa7637399cebba
Ramiro Menendez <ramiro99@live.com.ar>
2024-08-19 18:21:42 -0300

    a

--- Paradigmas/Funcional/TP3 v2.rkt
+++ Paradigmas/Funcional/TP3 v2.rkt
@@ -3,7 +3,12 @@
 (define agenda-semanal
   '((lunes ("Francisco Perez" 8 8.5) ("Mariano López" 9 9.5))
     (martes ("José Álvarez" 10 11) ("Fernanda Martínez" 11 12))
-    (miercoles) (jueves) (viernes)))
+    (miercoles) (jueves) (viernes ("Ramiro Menendez" 15 15.5))))
+
+(define agenda-semanal-1
+  '((lunes ("Fabián" 8 9) ("Laura" 9 9.5) ("Javier" 10 11) ("Cristian" 11 12) ("Cesira" 15 16) ("Paula" 16 16.5) ("L7" 16.5 17))
+    (martes ("Damián" 15.5 16) ("Milagros" 16 17))
+    (miercoles) (jueves ("Martina" 8.5 9)) (viernes ("Ramiro Menendez" 15.5 16))))
 
 (define horarios-inicio
   '(8 8.5 9 9.5 10 10.5 11 11.5 14 14.5 15 15.5 16 16.5) )
@@ -53,10 +58,14 @@
 
 (define lista-huecos
   (lambda(agenda)
+    (lista-huecos-2 agenda agenda)))
+
+(define lista-huecos-2
+  (lambda(agenda agenda-completa)
     (if (null? agenda)
         '()
-        (cons (list (caar agenda) (lista-huecos-dia(filtrar-horarios horarios-inicio (obtener-cronograma agenda-semanal (caar agenda)))))
-              (lista-huecos (cdr agenda))))))
+        (cons (list (caar agenda) (lista-huecos-dia(filtrar-horarios horarios-inicio (obtener-cronograma agenda-completa (caar agenda)))))
+              (lista-huecos-2 (cdr agenda) agenda-completa)))))
 
 ;esto junta en sublistas los horarios libres seguidos
 ;el reverse es para "contrarrestar" el hecho que las listas se construyen comenzando desde el final        
@@ -85,9 +94,64 @@
 
 ;-----------------------------------------------------------------
 ;PARTE 4
-(define reagendar-dia
-  (lambda(agenda-semanal
 
+(define reagendar-dia
+  (lambda(agenda dia)
+    (limpiar-dia-ocupado agenda dia (obtener-cronograma agenda dia))))
+
+;esto arma la nueva agenda insertando el cronograma del día a reagendar en el día que corresponde
+;si es viernes, llama a "nueva-agenda"
+
+(define limpiar-dia-ocupado
+  (lambda(agenda dia cronograma)
+    (cond ((null? agenda) '())
+          ((equal? dia (caar agenda)) (append (list(list(caar agenda))
+                                                   (cons (caar(cdr agenda)) (rearmar-dia (cdr(cadr agenda)) cronograma horarios-inicio)))
+                                              (limpiar-dia-ocupado (cddr agenda) dia cronograma)))
+          ((equal? dia 'viernes) (nueva-agenda cronograma))
+          (else (cons (car agenda) (limpiar-dia-ocupado (cdr agenda) dia cronograma))))))
+
+;se le pasa el cronograma existente y el que le queremos meter, junto con todos los horarios
+;devuelve un cronograma fusionado.
+
+(define rearmar-dia
+  (lambda(cronograma-existente cronograma-dado horarios-posibles)
+    (cond ((and (null? horarios-posibles)
+                ;(not (null? cronograma-existente))
+                (not (null? cronograma-dado)))
+           (error "'falla-asignacion" (cons (cadr (car cronograma-dado)) '())))
+          ((and (null? cronograma-existente)
+                (null? cronograma-dado)) '())
+        ;  ((null? cronograma-existente) rearmar-dia "(append cronograma-dado '())")
+          ((null? cronograma-dado) (append cronograma-existente '()))
+          (else (let ([tipo (if (= (+ 1 (cadr(car cronograma-dado))) (caddr(car cronograma-dado)))
+                                'complejo
+                                'estandar)])
+                  (if (not(turno-ocupado? cronograma-existente (car horarios-posibles) tipo))
+                      (cons (list (caar cronograma-dado) (car horarios-posibles) (if (equal? tipo 'complejo)
+                                                                                     (+ 1 (car horarios-posibles))
+                                                                                     (+ 0.5 (car horarios-posibles))))
+                            (rearmar-dia cronograma-existente (cdr cronograma-dado) (cdr horarios-posibles)))
+
+                      (if (= (car horarios-posibles) (cadr(car cronograma-existente)))
+                          (cons (car cronograma-existente) (rearmar-dia (cdr cronograma-existente) cronograma-dado
+                                                                        (cdr horarios-posibles)))
+                          (rearmar-dia cronograma-existente cronograma-dado (cdr horarios-posibles)))))))))
+
+
+                   
+                                                        
+         
+
+;esto arma una nueva agenda semanal con los horarios en el lunes
+;(los reagendados del viernes)
+(define nueva-agenda
+  (lambda(cronograma)
+    (cons (append (list 'lunes) cronograma)
+          (cons (list 'martes) 
+                (cons (list 'miercoles)
+                      (cons (list 'jueves)
+                            (cons (list 'viernes) '())))))))
 
 ;-----------------------------------------------------------------
 ;obtener-horario
@@ -111,10 +175,7 @@
           ((equal? 'miercoles dia) (cdr (caddr agenda)))
           ((equal? 'jueves dia) (cdr (cadddr agenda)))
           ((equal? 'viernes dia) (cdr (car (cddddr agenda))))
-          (else (error "ta mal"))
-          )
-    )
-  )
+          (else (error "ta mal")))))
 
 ;----------------------------------------------------------------------
 ;turno-ocupado? devuelve #f si se puede reservar y #t si no es posible.
@@ -140,13 +201,3 @@
                              (= horario 11.5)
                              (= horario 16.5))))       
                  (turno-ocupado? (cdr cronograma-dia) horario tipo))))))
-
-
-
-
-
-
-
-
-
-
